ae9cc312295a1701f56a70ad6e7cc65602b16a88
Robert Estey <RWE001@perfect.com>
2019-06-17 00:22:57 -0400

    adding JDBC example

--- jdk8/src/jdbc/JDBCExample.java
+++ jdk8/src/jdbc/JDBCExample.java
@@ -2,12 +2,15 @@
 package jdbc;
 
 import java.sql.DriverManager;
+import java.sql.ResultSet;
 import java.sql.Connection;
 import java.sql.SQLException;
+import java.sql.Statement;
 
 public class JDBCExample {
 
-	private static final String DATABASE = "jdbc:mysql://localhost:3306/world";
+	// private static final String DATABASE = "jdbc:mysql://localhost:3306/mysql";
+	private static final String DATABASE = "jdbc:mariadb://localhost:3306/mysql";
 	private static final String USERNAME = "root";
 	private static final String PASSWORD = "root";
 
@@ -16,7 +19,8 @@ public class JDBCExample {
 		System.out.println("-------- MySQL JDBC Connection Testing ------------");
 
 		try {
-			Class.forName("com.mysql.jdbc.Driver");
+			// Class.forName("com.mysql.jdbc.Driver");
+			Class.forName("org.mariadb.jdbc.Driver");
 		} catch (ClassNotFoundException e) {
 			System.out.println("Where is your MySQL JDBC Driver?");
 			e.printStackTrace();
@@ -25,20 +29,29 @@ public class JDBCExample {
 
 		System.out.println("MySQL JDBC Driver Registered!");
 		Connection connection = null;
+		String sql = "select user from user";
 
 		try {
 			connection = DriverManager.getConnection(DATABASE, USERNAME, PASSWORD);
+			Statement statement = connection.createStatement();
+
+			ResultSet resultSet = statement.executeQuery(sql);
+			while (resultSet.next()) {
+
+				String username = resultSet.getString("User");
+				System.out.println("Username: " + username);
+			}
+
+			if (connection != null) {
+				System.out.println("You made it, take control your database now!");
+			} else {
+				System.out.println("Failed to make connection!");
+			}
 
 		} catch (SQLException e) {
 			System.out.println("Connection Failed! Check output console");
 			e.printStackTrace();
 			return;
-		}
-
-		if (connection != null) {
-			System.out.println("You made it, take control your database now!");
-		} else {
-			System.out.println("Failed to make connection!");
 		}
 	}
 }
